34017fb7e4bbdc5e992bbbc08f29e57cf5313d05
Matheus Pessino <paulomatheuspessino@gmail.com>
2022-11-17 15:27:22 -0300

    Criação do service de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { HotToastModule } from '@ngneat/hot-toast';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      progressBar: true,
+      closeButton: true
+    }),
+    HotToastModule.forRoot(),
+    HttpClientModule,
+    JwtModule
   ],
   providers: [],
   bootstrap: [AppComponent]
